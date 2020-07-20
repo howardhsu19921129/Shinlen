ebff3cd70e29e8e9c5d39a4ea329203cf9066316
Lee Shiueh <lee.shiueh@flh.com.tw>
2020-07-20 16:27:38 +0800

    reset pi, test commit

---
+++ event/list_event.py
@@ -0,0 +1,13 @@
+import sys, os, json
+
+def get_json_data(json_path):
+	with open(json_path,'r') as f:  
+		params = json.load(f)      
+		dict = params  
+		f.close()
+	return dict
+
+def list_action(list_path):
+	load_data = get_json_data(list_path)
+	for i in load_data['event_list']:
+		print(i)
